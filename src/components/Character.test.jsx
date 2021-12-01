db13992b69c827495b0e9babb00a21663c3c01be
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-11-30 18:07:57 -0800

    added failing character test

---
+++ src/components/Character.test.jsx
@@ -0,0 +1,18 @@
+// import { screen, render } from "@testing-library/react";
+// import Character from "./Character";
+
+// it("should display the Character component", () => {
+//   const { container } = render(
+//     <Character head="Skeksi" body="Skeksi" accessory="Shard" />
+//   );
+
+//   const head = screen.getByLabelText(/Head/);
+//   const body = screen.getByLabelText(/Body/);
+//   const accessory = screen.getByLabelText(/Accessorize!/);
+
+//   expect(head).toHaveStyle(`background-image: url(./Skeksi-head.png)`);
+//   expect(body).toHaveStyle(`background-image: url(./Skeksi-body.png)`);
+//   expect(accessory).toHaveStyle(`background-image: url(./Shard-accessory.png)`);
+
+//   expect(container).toMatchSnapshot();
+// });
